3f681fe6be11bb3710cee139bc0db8079888c373
Kang Shiang Yap <KangShiang@Kangs-MacBook-Pro.local>
2020-03-24 22:49:41 -0700

    added static dir

---
+++ static/precache-manifest.895d08de170baa3a528049f93ddd618e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa439d043b9a7390b4edcee28c66ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80dbca8556b41bed5fc1",
+    "url": "/static/css/main.1ded1b37.chunk.css"
+  },
+  {
+    "revision": "051d0a2407da5801c9d4",
+    "url": "/static/js/2.9d3c5edf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9d3c5edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80dbca8556b41bed5fc1",
+    "url": "/static/js/main.fe6aff29.chunk.js"
+  },
+  {
+    "revision": "e91bb28b81e0b7a50ff8",
+    "url": "/static/js/runtime-main.11799508.js"
+  }
+]);
